0e16728611c1af859274780adbfc80b8331461bf
Chris_Jarvis <clj@ollure.com>
2015-11-14 11:26:08 -0600

    added board controller

---
+++ Brello.Tests/Controllers/BoardContollerTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brello.Controllers;
+using System.Web.Mvc;
+
+namespace Brello.Tests.Controllers
+{
+    [TestClass]
+    public class BoardContollerTest
+    {
+        [TestMethod]
+        public void BoardControllerEnsureIndexPageExist()
+        {
+            //Begin Arrange
+            BoardController controller = new BoardController();
+            //End Arrange
+
+            //Begin Act
+           ViewResult result = controller.Index() as ViewResult;
+            //End Act
+
+            //Begin Assert
+            Assert.IsNotNull(result);
+            //End Assert
+        }
+
+        [TestMethod]
+        public void EnsureIndexViewExist()
+        {
+            //Begin Arrange
+            BoardController controller = new BoardController();
+            //End Arrange
+
+            //Begin Act
+            ViewResult result = controller.Index() as ViewResult;
+            //End Act
+
+            //Begin Assert
+            Assert.AreEqual( "Index", result.ViewName);
+            //End Assert    
+        }
+        [TestMethod]
+        public void BoardControllerEnsureItHasThings()
+        {
+            BoardController controller = new BoardController();
+            //End Arrange
+
+            //Begin Act
+            ViewResult result = controller.Index() as ViewResult;
+            //End Act
+
+            //Begin Assert
+            string expected_message = "My Boards";
+            Assert.AreEqual(expected_message, result.ViewBag.Message);
+            //End Assert
+        }
+    }
+}
